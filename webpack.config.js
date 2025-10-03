4194fa20d4978aecca515bae7e543af646ac496d
Isak Grönlund <135023164+Grizak@users.noreply.github.com>
2025-10-03 08:15:00 +0200

    feat(build): minification

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+const mainConfig = {
+  entry: './src/main.js',
+  target: 'electron-main',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  node: {
+    __dirname: false,
+    __filename: false
+  },
+  optimization: {
+    minimizer: [new TerserPlugin({
+      terserOptions: {
+        compress: {
+          drop_console: false,
+        }
+      }
+    })]
+  }
+};
+
+const preloadConfig = {
+  entry: './src/preload.js',
+  target: 'electron-preload',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'preload.js'
+  }
+};
+
+const rendererConfig = {
+  target: 'electron-renderer',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/renderer.html',
+      filename: 'renderer.html',
+      inject: false,
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true
+      }
+    })
+  ]
+};
+
+module.exports = [mainConfig, preloadConfig, rendererConfig];
