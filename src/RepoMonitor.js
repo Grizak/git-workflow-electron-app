2875d9082d17003d28f5d6717c221b2179def291
Isak Grönlund <gronlundisak11@gmail.com>
2025-10-05 07:45:53 +0200

    feat: Automatic reload on change

--- src/RepoMonitor.js
+++ src/RepoMonitor.js
@@ -3,6 +3,27 @@ const chokidar = require("chokidar");
 const simpleGit = require("simple-git");
 const path = require("path");
 
+const onChange = async function (filePath) {
+  try {
+    const status = await this.git.status();
+
+    // Check if there are any actual git changes
+    const hasChanges =
+      status.modified.length > 0 ||
+      status.created.length > 0 ||
+      status.deleted.length > 0 ||
+      status.not_added.length > 0 ||
+      status.conflicted.length > 0 ||
+      status.staged.length > 0;
+
+    if (hasChanges) {
+      this.emit("file-changed", this.getRelativePath(filePath));
+    }
+  } catch (error) {
+    console.error("Error checking git status:", error);
+  }
+};
+
 class RepoMonitor extends EventEmitter {
   constructor(repoPath) {
     super();
@@ -22,16 +43,12 @@ class RepoMonitor extends EventEmitter {
       },
     });
 
+    const boundOnChange = onChange.bind(this);
+
     this.watcher
-      .on("add", (filePath) => {
-        this.emit("file-changed", this.getRelativePath(filePath));
-      })
-      .on("change", (filePath) => {
-        this.emit("file-changed", this.getRelativePath(filePath));
-      })
-      .on("unlink", (filePath) => {
-        this.emit("file-changed", this.getRelativePath(filePath));
-      });
+      .on("add", boundOnChange)
+      .on("change", boundOnChange)
+      .on("unlink", boundOnChange);
   }
 
   getRelativePath(filePath) {
